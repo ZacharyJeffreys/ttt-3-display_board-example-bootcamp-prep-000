75550ed5b1b78b8ac802d5eb148695b2d6ebffdd
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-31 04:15:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  cells = ["   |   |   "]
-  dash = ["-----------"]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
   
-  print cells
-  print dash
-  print cells
-  print dash
-  print cells
+
 end
